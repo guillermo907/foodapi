df8bff6de7aad7d6ac02aed3ac7c61dbe16bf9c7
Veronica <vero@192.168.1.6>
2023-03-17 12:48:56 -0600

    cambios desde gomez

--- getFoods.php
+++ getFoods.php
@@ -2,6 +2,15 @@
 // Include the database connection file
 include "datacon.php";
 
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+$method = $_SERVER['REQUEST_METHOD'];
+if ($method == "OPTIONS") {
+    die();
+}
+
 // Retrieve data from the table
 $sql = "SELECT * FROM foods";
 $result = mysqli_query($conn, $sql);
